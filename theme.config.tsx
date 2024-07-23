7f4549d0e43a630536c12b6b713e4a7891f26ecc
Madoka <forevercarrot@sina.cn>
2024-07-23 23:15:57 +0800

    complete index

--- theme.config.tsx
+++ theme.config.tsx
@@ -1,18 +1,50 @@
 import React from 'react'
 import { DocsThemeConfig } from 'nextra-theme-docs'
+import { useRouter } from 'next/router'
+ 
+// 图标
+const ayaLogo = () => (
+  <div style={{ display: 'flex', alignItems: 'center' }}>
+    {/* 引用public目录下的favicon.ico */}
+    <img src="/favicon.ico" alt="ayaLogo" style={{ width: '32px', height: '32px', marginRight: '10px' }} />
+    <span>Aya 帮助文档</span>
+  </div>
+);
 
 const config: DocsThemeConfig = {
-  logo: <span>My Project</span>,
+  logo: ayaLogo,
   project: {
-    link: 'https://github.com/shuding/nextra-docs-template',
+    link: 'https://github.com/madoka315/ayahelp2',
   },
+  // 社交媒体
   chat: {
     link: 'https://discord.com',
+    icon: (
+      <img src="/tencent-qq.svg" alt="qq"></img>
+    )
   },
-  docsRepositoryBase: 'https://github.com/shuding/nextra-docs-template',
+  docsRepositoryBase: 'https://github.com/madoka315/ayahelp2',
+  // 主题颜色
+  primaryHue: 5,
+  // 禁用反馈和编辑链接
+  feedback: {
+    content: null
+  },
+  editLink: {
+    component: null
+  },
   footer: {
-    text: 'Nextra Docs Template',
+    text: 'Aya 帮助文档',
   },
+  useNextSeoProps() {
+    // 自定义标题
+    const { asPath } = useRouter()
+    if (asPath !== '/') {
+      return {
+        titleTemplate: '%s – Aya 帮助文档'
+      }
+    }
+  }
 }
 
 export default config
